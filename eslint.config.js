58011ffa998f26b62dbe47aaca195fb5a64c76b8
cwchanap <cwchanap@connect.ust.hk>
2025-07-26 18:00:52 -0700

    feat: setup eslint, husky, and lint-staged for the monorepo

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import { config } from "@repo/eslint-config/base";
+
+export default [
+  ...config,
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.turbo/**",
+      "**/.output/**",
+      "**/.wxt/**",
+      "**/.vinxi/**",
+    ],
+  },
+];
