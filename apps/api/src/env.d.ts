f2137781bb0880531a80a98150fa42afa3e666ae
cwchanap <cwchanap@connect.ust.hk>
2025-12-23 19:31:25 -0800

    fix(api): bundle graphql schema

--- apps/api/src/env.d.ts
+++ apps/api/src/env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-declare module "*.graphql?raw" {
-  const content: string;
-  export default content;
+declare module "*.graphql" {
+    const content: string;
+    export default content;
 }
