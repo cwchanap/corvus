a404923c80683e21ed0cf6c6fabb9ba2368dd951
cwchanap <cwchanap@connect.ust.hk>
2026-04-03 14:38:42 -0700

    fix: allow underscore-prefixed unused vars in ESLint and update tests

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -32,6 +32,14 @@ export const config = [
     },
   },
   {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
     ignores: ["dist/**", ".wxt/**", ".vinxi/**", ".output/**"],
   },
 ];
