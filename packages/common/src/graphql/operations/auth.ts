9e2fd7e22fb3038c95aab1e633b00e8c264880d3
cwchanap <cwchanap@connect.ust.hk>
2026-05-20 11:58:50 -0700

    refactor: remove password auth graphql operations

--- packages/common/src/graphql/operations/auth.ts
+++ packages/common/src/graphql/operations/auth.ts
@@ -4,12 +4,7 @@
  */
 
 import { graphqlRequest, type GraphQLClientOptions } from "../client";
-import type {
-  GraphQLUser,
-  AuthPayload,
-  RegisterInput,
-  LoginInput,
-} from "../types";
+import type { GraphQLUser } from "../types";
 
 // Query strings
 export const ME_QUERY = `
@@ -24,38 +19,6 @@ export const ME_QUERY = `
   }
 `;
 
-export const REGISTER_MUTATION = `
-  mutation Register($input: RegisterInput!) {
-    register(input: $input) {
-      success
-      user {
-        id
-        email
-        name
-        createdAt
-        updatedAt
-      }
-      error
-    }
-  }
-`;
-
-export const LOGIN_MUTATION = `
-  mutation Login($input: LoginInput!) {
-    login(input: $input) {
-      success
-      user {
-        id
-        email
-        name
-        createdAt
-        updatedAt
-      }
-      error
-    }
-  }
-`;
-
 export const LOGOUT_MUTATION = `
   mutation Logout {
     logout
@@ -64,47 +27,23 @@ export const LOGOUT_MUTATION = `
 
 // Operation functions
 export async function getCurrentUser(
-  options?: Partial<GraphQLClientOptions>,
+    options?: Partial<GraphQLClientOptions>,
 ): Promise<GraphQLUser | null> {
-  const data = await graphqlRequest<{ me: GraphQLUser | null }>(
-    ME_QUERY,
-    undefined,
-    options,
-  );
-  return data.me;
-}
-
-export async function register(
-  input: RegisterInput,
-  options?: Partial<GraphQLClientOptions>,
-): Promise<AuthPayload> {
-  const data = await graphqlRequest<{ register: AuthPayload }>(
-    REGISTER_MUTATION,
-    { input },
-    options,
-  );
-  return data.register;
-}
-
-export async function login(
-  input: LoginInput,
-  options?: Partial<GraphQLClientOptions>,
-): Promise<AuthPayload> {
-  const data = await graphqlRequest<{ login: AuthPayload }>(
-    LOGIN_MUTATION,
-    { input },
-    options,
-  );
-  return data.login;
+    const data = await graphqlRequest<{ me: GraphQLUser | null }>(
+        ME_QUERY,
+        undefined,
+        options,
+    );
+    return data.me;
 }
 
 export async function logout(
-  options?: Partial<GraphQLClientOptions>,
+    options?: Partial<GraphQLClientOptions>,
 ): Promise<boolean> {
-  const data = await graphqlRequest<{ logout: boolean }>(
-    LOGOUT_MUTATION,
-    undefined,
-    options,
-  );
-  return data.logout;
+    const data = await graphqlRequest<{ logout: boolean }>(
+        LOGOUT_MUTATION,
+        undefined,
+        options,
+    );
+    return data.logout;
 }
